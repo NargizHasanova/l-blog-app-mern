6cc4756da2af26646beec3844b6531195d1d38d0
NargizHasanova <nargiz.frontdev3@gmail.com>
2022-09-15 09:27:29 +0400

    commit

---
+++ api/routes/posts.js
@@ -0,0 +1,93 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const Post = require("../models/Post");
+
+// CREATE POST
+router.post("/", async (req, res) => {
+    try {
+        const newPost = new Post(req.body)
+        const createdPost = await newPost.save();
+        return res.status(200).json(createdPost);
+    } catch (err) {
+        return res.status(500).json(err.message)
+    }
+})
+
+// UPDATE POST
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (post.username === req.body.username) {
+            try {
+                const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
+                    $set: req.body
+                }, { new: true }) // { new: true } => to see updated version on postman response
+
+                return res.status(200).json(updatedPost)
+            } catch (err) {
+                return res.status(500).json(err.message)
+            }
+        } else {
+            return res.status(401).json("You can only update your post!")
+        }
+    } catch (err) {
+        return res.status(500).json(err.message)
+    }
+})
+
+// DELETE POST
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username) {
+            try {
+                await post.delete();
+                return res.status(200).json("Post has been deleted...");
+            } catch (err) {
+                return res.status(500).json(err.message);
+            }
+        } else {
+            return res.status(401).json("You can delete only your post!");
+        }
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
+// GET POST
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        return res.status(200).json(post)
+    } catch (err) {
+        return res.status(500).json(err.message)
+    }
+})
+
+// GET ALL POSTS
+router.get("/", async (req, res) => {
+    const username = req.query.user
+    const catName = req.query.cat
+    try {
+        let posts
+        if (username) {
+            posts = await Post.find({
+                username: username
+            })
+        } else if (catName) {
+            posts = await Post.find({
+                categories: {
+                    $in: [catName] // $in = inside,includes
+                }
+            })
+        } else {
+            posts = await Post.find() // getting all posts ,/without condition
+        }
+        return res.status(200).json(posts)
+    } catch (err) {
+        return res.status(500).json(err.message)
+    }
+})
+
+
+module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -29,19 +29,28 @@ router.delete("/:id", async (req, res) => {
             const user = await User.findById(req.params.id);
             try {
                 await Post.deleteMany({ username: user.username });
-                await User.findByIdAndDelete(req.params.id);
-                res.status(200).json("User has been deleted...");
+                await User.findByIdAndDelete(req.params.id); // user.delete()
+                return res.status(200).json("User has been deleted...");
             } catch (err) {
-                res.status(500).json(err);
+                return res.status(500).json(err.message);
             }
         } catch (err) {
-            res.status(404).json("User not found!");
+            return res.status(404).json("User not found!");
         }
     } else {
-        res.status(401).json("You can delete only your account!");
+        return res.status(401).json("You can delete only your account!");
     }
 });
 
 //GET USER
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        const { password, ...others } = user._doc // gelen response-da password yeride gelmesin
+        return res.status(200).json(others)
+    } catch (err) {
+        return res.status(500).json(err.message)
+    }
+})
 
 module.exports = router;
